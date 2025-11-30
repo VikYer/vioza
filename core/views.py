f7f5041e8d778c666e6e5329437ca931bea5da73
VikYer <piligrimxix@gmail.com>
2025-11-30 13:27:39 +0100

    feat(search): add preliminary search template skeleton

--- core/views.py
+++ core/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+VOIVODESHIPS = [
+    "Dolnośląskie", "Kujawsko-pomorskie", "Lubelskie", "Lubuskie",
+    "Łódzkie", "Małopolskie", "Mazowieckie", "Opolskie",
+    "Podkarpackie", "Podlaskie", "Pomorskie", "Śląskie",
+    "Świętokrzyskie", "Warmińsko-mazurskie", "Wielkopolskie",
+    "Zachodniopomorskie",
+]
+
 def index(request):
     return render(request,
                   'core/index.html',
-                  {'title':'Main'})
+                  {'title':'Main',
+                   'voivodeships': VOIVODESHIPS})
