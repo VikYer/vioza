2a672dd1d9a54ed8443ffcff3a5a6bbdbecea302
VikYer <piligrimxix@gmail.com>
2025-12-01 22:17:46 +0100

    feat(ad model): register ad model in admin panel

--- classifieds/admin.py
+++ classifieds/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Ad
+
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'category', 'publish', 'is_promoted', 'views_qty',)
+    list_filter = ('category', 'is_promoted', 'status', 'publish',)
+    search_fields = ('title', 'description', 'author__username')
+    ordering = ('-publish',)
+    list_display_links = ('title',)
+    readonly_fields = ('views_qty', 'status', 'created', 'publish', 'updated', )
+    list_editable = ('is_promoted',)
