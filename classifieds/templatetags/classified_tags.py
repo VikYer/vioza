ba81817db2ad350a406c04e21b81ab87296f2f1f
VikYer <piligrimxix@gmail.com>
2025-12-14 16:49:17 +0100

    feat(classifieds): add displaying subcategories under category card

--- classifieds/templatetags/classified_tags.py
+++ classifieds/templatetags/classified_tags.py
@@ -6,5 +6,9 @@ register = template.Library()
 
 @register.inclusion_tag('classified/includes/_category_list.html')
 def display_category_list():
-    categories = Category.objects.only('title', 'icon').order_by('title')
+    categories = (
+        Category.objects
+        .prefetch_related('subcategories')
+        .order_by('title')
+    )
     return {'categories': categories}
