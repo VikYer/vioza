7e93c856727af3693705867f342ee4c16c506421
VikYer <piligrimxix@gmail.com>
2026-01-21 19:55:57 +0100

    feat(classifieds.views): add AdListView with filtering by category and subcategory slugs

--- classifieds/views.py
+++ classifieds/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Ad, Category, Subcategory
+
+
+class AdListView(ListView):
+    model = Ad
+    template_name = 'classifieds/ads_list.html'
+    context_object_name = 'ads'
+
+    def get_queryset(self):
+        queryset = Ad.published.select_related('category', 'subcategory')
+
+        self.category = None
+        self.subcategory = None
+
+        category_slug = self.kwargs.get('category_slug')
+        subcategory_slug = self.kwargs.get('subcategory_slug')
+
+        if category_slug:
+            self.category = get_object_or_404(Category, slug=category_slug)
+            queryset = queryset.filter(category=self.category)
+
+        if subcategory_slug:
+            self.subcategory = get_object_or_404(Subcategory, slug=subcategory_slug)
+            queryset = queryset.filter(subcategory=self.subcategory)
+
+        return queryset.order_by('-publish')
